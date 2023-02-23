9809e0e5cf4e3e60802c2b001049b5d92b32f16a
ali shojaei far <107762076+icoderclick@users.noreply.github.com>
2023-02-23 12:04:40 +0330

    Update add_menu_admin.php

--- utility/add_menu_admin.php
+++ utility/add_menu_admin.php
@@ -1,11 +1,14 @@
+
+this funcs for add menu admin and delete first submenu
+
 <?
 
-#this funcs for add menu admin and delete first submenu
-
-
-
+//step 1: set hook admin_menu | f.e:
 add_action('admin_menu', 'plugin_filter_link_register_menu');
 
+
+
+//step 2: add menu and submenu for apis add_menu_page,add_submenu_page and delete first submenu with remove_submenu_page api | f.e:
 function plugin_filter_link_register_menu()
 {
     add_menu_page(
@@ -19,7 +22,7 @@ function plugin_filter_link_register_menu()
     );
 
 
-    #setting page
+    // add submenu page
     add_submenu_page(
         'filter-link-main',
         'تنظیمات',
@@ -29,17 +32,22 @@ function plugin_filter_link_register_menu()
         'filter_link_page_setting'
     );
     
+    
+    // remove submenu page
     remove_submenu_page('filter-link-main', 'filter-link-main');
 }
 
+//callback functions (views)
 
 function filter_link_page_dashboard()
 {
     echo 'this is dashboard page';
 }
-
-
 function filter_link_page_setting()
 {
     echo 'this is setting page';
 }
+
+
+
+
