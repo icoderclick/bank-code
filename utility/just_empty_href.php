b95b094b486f48d176f9389672d31fdf5b39c787
ali shojaei far <107762076+icoderclick@users.noreply.github.com>
2023-02-23 10:56:52 +0330

    Create just_empty_href.php

---
+++ utility/just_empty_href.php
@@ -0,0 +1,10 @@
+#empty just href
+function empty_just_href($comment, $status = true)
+{
+    if (!$status)
+        return $comment;
+
+    return preg_replace('/href="[^"]*"/', 'href=" "', $comment);
+
+}
+add_filter('comment_text', 'empty_just_href');
