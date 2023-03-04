4788bd81fe19087d9539cc7e9b6493bf2024ad2f
ali shojaei far <107762076+icoderclick@users.noreply.github.com>
2023-03-04 18:29:18 +0330

    Create all_roles_and_capbility.php

---
+++ utility/all_roles_and_capbility.php
@@ -0,0 +1,89 @@
+<?php
+
+A list of all possible Capabilities per user roles:
+https://wordpress.org/support/article/roles-and-capabilities/#capability-vs-role-table
+
+Super Admin
+=========================
+create_sites
+delete_sites
+manage_network
+manage_sites
+manage_network_users
+manage_network_plugins
+manage_network_themes
+manage_network_options
+upload_plugins
+upload_themes
+upgrade_network
+setup_network
+
+Super Admin + Administrator
+==========================================
+activate_plugins (single site or enabled by network setting)
+create_users (single site)
+delete_plugins (single site)
+delete_themes (single site)
+delete_users (single site)
+edit_files (single site)
+edit_plugins (single site)
+edit_theme_options
+edit_themes (single site)
+edit_users (single site)
+export
+import
+
+Super Admin + Administrator
+=============================================
+install_plugins (single site)
+install_themes (single site)
+list_users
+manage_options
+promote_users
+remove_users
+switch_themes
+update_core (single site)
+update_plugins (single site)
+update_themes (single site)
+edit_dashboard
+customize
+delete_site
+
+Super Admin + Administrator + Editor
+====================================================
+moderate_comments
+manage_categories
+manage_links
+edit_others_posts
+edit_pages
+edit_others_pages
+edit_published_pages
+publish_pages
+delete_pages
+delete_others_pages
+delete_published_pages
+delete_others_posts
+delete_private_posts
+edit_private_posts
+read_private_posts
+delete_private_pages
+edit_private_pages
+read_private_pages
+unfiltered_html (single site)
+unfiltered_html
+
+Super Admin + Administrator + Editor + Author
+==========================================================
+edit_published_posts
+upload_files
+publish_posts
+delete_published_posts
+
+Super Admin + Administrator + Editor + Author + Contributor
+========================================================================
+edit_posts
+delete_posts
+
+Super Admin + Administrator + Editor + Author + Contributor + Subscriber
+======================================================================================
+read
